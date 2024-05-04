69436e01b000a8367f43277e8bf8f31078d01531
Tommaso Clini <tommaso@localhost.localdomain>
2024-05-04 17:44:53 +0200

    parabolic function

--- esp32_f1_car/main/main.cpp
+++ esp32_f1_car/main/main.cpp
@@ -10,8 +10,6 @@
 
 #include <algorithm>
 
-#define MAP(x, l0, h0, l1, h1) (((h1 - l1) / (h0 - l0)) * (x - l0) + l1)
-
 static const char *TAG = "main";
 
 static bool limiter_active;
@@ -21,23 +19,43 @@ static const float duty_min = 0.05f;
 static const float duty_max = 0.10f;
 static const float duty_mid = (duty_min + duty_max) / 2.0f;
 
-#define THROTTLE_COEFF_DEFAULT_VAL      0.15f // min 0.0 max 1.0
-#define THROTTLE_COEFF_1_DEFAULT_VAL    0.21f
-#define THROTTLE_NOS_COEFF              1.5f
-#define BRAKE_COEFF_DEFAULT_VAL         0.6f // min 0.0 max 1.0
+static float a, b, c;
 
-static float throttle_coeff = THROTTLE_COEFF_DEFAULT_VAL;
-static float throttle_coeff_1 = THROTTLE_COEFF_1_DEFAULT_VAL;
-static float brake_coeff = BRAKE_COEFF_DEFAULT_VAL;
+static const float brake_coeff = 0.6f;
 
 pwm_capture::pwm_cap st_cap(GPIO_NUM_0, "gpio 0 st pwm cap");
 pwm_capture::pwm_cap th_cap(GPIO_NUM_1, "gpio 1 th pwm cap");
 pwm_capture::pwm_cap ch3_cap(GPIO_NUM_2, "gpio 2 ch3 pwm cap");
 pwm_capture::pwm_cap ch4_cap(GPIO_NUM_3, "gpio 3 ch4 pwm cap");
 
+template<typename T>
+static T map(T x, T l0, T h0, T l1, T h1){
+    return (((h1 - l1) / (h0 - l0)) * (x - l0) + l1);
+}
+
+template<typename T>
+void parameters(T *a, T *b, T *c, T x1, T y1, T x2, T y2, T x3, T y3){
+    T denom = (x1 - x2) * (x1 - x3) * (x2 - x3);
+    *a     = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / denom;
+    *b     = (x3*x3 * (y1 - y2) + x2*x2 * (y3 - y1) + x1*x1 * (y2 - y3)) / denom;
+    *c     = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / denom;
+}
+
+template<typename T>
+static T process_duty(T duty){
+    float x = std::abs(duty - duty_mid);
+    if (duty >= duty_mid)
+    {
+        duty = duty_mid + a * x*x + b * x + c;
+    } else {
+        duty = duty_mid - x * brake_coeff;
+    }
+    return duty;
+}
 
 extern "C" void app_main(void)
 {
+    parameters(&a, &b, &c, 0.0f, 0.0f, 0.0125f, 0.0025f, 0.025f, 0.006f);
     ESP_ERROR_CHECK(init_params());
     
     ESP_ERROR_CHECK(get_limiter_p(&limiter_p));
@@ -59,7 +77,7 @@ extern "C" void app_main(void)
     st_cap.start();
     th_cap.start();
     ch3_cap.start();
-    ch4_cap.start();
+    // ch4_cap.start();
 
     gpio_num_t st_gpio = st_cap.get_gpio();
     gpio_num_t th_gpio = th_cap.get_gpio();
@@ -76,46 +94,19 @@ extern "C" void app_main(void)
         if (pwm.gpio == th_gpio)
         {
             float duty = std::clamp((float)pwm.duty / (float)pwm.period, duty_min, duty_max);
-            if (duty >= duty_mid){
-                // th = MAP(duty, duty_min, duty_max, (float)(0x7fff - THROTTLE_RANGE), (float)(0x7fff + THROTTLE_RANGE));
-                float x = (duty - duty_mid);
-                if (x <= (duty_max + duty_mid) / 2.0f)
-                {
-                    duty = duty_mid + throttle_coeff * x;
-                } else {
-                    duty = duty_mid + throttle_coeff_1 * x;
-                }
-            } else {
-                // th = MAP(duty, duty_min, duty_max, (float)(0x0), (float)(0xffff));
-                duty = duty_mid - (duty_mid - duty) * brake_coeff;
-            }
-            th = MAP(duty, duty_min, duty_max, (float)0x0, (float)0xffff);
+            
+            duty = process_duty(duty);
+
+            th = map(duty, duty_min, duty_max, (float)0x0, (float)0xffff);
         }
         else if (pwm.gpio == st_gpio)
         {
-            st = MAP(std::clamp((float)pwm.duty / (float)pwm.period, duty_min, duty_max), duty_min, duty_max, (float)0xffff, (float)0x0);
+            st = map(std::clamp((float)pwm.duty / (float)pwm.period, duty_min, duty_max), duty_min, duty_max, (float)0xffff, (float)0x0);
             steering_servo_write_u16(st);
         }
         else if (pwm.gpio == ch3_gpio)
         {
             limiter_active = ((float)pwm.duty / (float)pwm.period) > duty_mid;
-        }
-        else if (pwm.gpio == ch4_gpio)
-        {
-            static bool last_boost;
-            bool boost = ((float)pwm.duty / (float)pwm.period) >= duty_mid;
-            if (boost != last_boost)
-            {
-                if (boost)
-                {
-                    throttle_coeff = std::min(THROTTLE_COEFF_DEFAULT_VAL * THROTTLE_NOS_COEFF, 1.0f);
-                }
-                else
-                {
-                    throttle_coeff = THROTTLE_COEFF_DEFAULT_VAL;
-                }
-            }
-            last_boost = boost;
         }
 
         if (limiter_active)
